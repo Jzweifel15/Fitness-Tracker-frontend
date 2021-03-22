57f3a2d9a47ab5be9ab4636a500941cc18fe4cd7
Jzweifel15 <jzweifel15@yahoo.com>
2021-03-22 01:43:13 -0400

    Update Navbar.js
    Reorganized the code of the Navbar component to make it easier to read and understand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,10 +9,18 @@ export const Navbar = () => {
         <h2>Fitness Tracker</h2>
       </div>
       <div className="navbar-links">
-        <a href="https://github.com/" target="_blank"><i className="fab fa-github"></i></a>
-        <a href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook"></i></a>
-        <a href="https://www.twitter.com/" target="_blank"><i className="fab fa-twitter"></i></a>
-        <a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a>
+        <a href="https://github.com/" target="_blank" rel="noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
+          <i className="fab fa-facebook"></i>
+        </a>
+        <a href="https://www.twitter.com/" target="_blank" rel="noreferrer">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
         <LogoutContainer />
       </div>
     </nav>
