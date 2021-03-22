cadda2bb962fda666d198a6c59d6bdd926aa6e19
Jzweifel15 <jzweifel15@yahoo.com>
2021-03-22 01:40:24 -0400

    Update Navbar.js
    Changed the Links out to anchors due to the Links not opening the links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import LogoutContainer from "./LogoutContainer";
 import "../styles/Navbar.css";
 
@@ -10,10 +9,10 @@ export const Navbar = () => {
         <h2>Fitness Tracker</h2>
       </div>
       <div className="navbar-links">
-        <Link to="https://github.com/"><i className="fab fa-github"></i></Link>
-        <Link to="https://www.facebook.com/"><i className="fab fa-facebook"></i></Link>
-        <Link to="https://www.twitter.com/"><i className="fab fa-twitter"></i></Link>
-        <Link to="https://www.instagram.com/"><i className="fab fa-instagram"></i></Link>
+        <a href="https://github.com/" target="_blank"><i className="fab fa-github"></i></a>
+        <a href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook"></i></a>
+        <a href="https://www.twitter.com/" target="_blank"><i className="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram"></i></a>
         <LogoutContainer />
       </div>
     </nav>
